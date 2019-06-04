2fb1edfab8ed815a270330f3d55dac644ac5d403
vincent-duan <1621424380@qq.com>
2019-06-05 01:46:45 +0800

    update

--- src/main/java/cn/ac/iie/App.java
+++ src/main/java/cn/ac/iie/App.java
@@ -2,10 +2,13 @@ package cn.ac.iie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+
+        ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
+        run.getBean("createClient");
     }
 }
---
+++ src/main/java/cn/ac/iie/bean/EsConf.java
@@ -0,0 +1,52 @@
+package cn.ac.iie.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties("es")
+public class EsConf {
+
+//    @Value("${es.ips}")
+    private List<String> ips;
+//    @Value("${es.clusterName}")
+    private String clusterName;
+//    @Value("${es.port}")
+    private int port;
+
+    @Override
+    public String toString() {
+        return "EsConf{" +
+                "ips=" + ips +
+                ", clusterName='" + clusterName + '\'' +
+                ", port=" + port +
+                '}';
+    }
+
+    public List<String> getIps() {
+        return ips;
+    }
+
+    public String getClusterName() {
+        return clusterName;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setIps(List<String> ips) {
+        this.ips = ips;
+    }
+
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
