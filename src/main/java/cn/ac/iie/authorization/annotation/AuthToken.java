c3a0c67a5455e0ab699edda22f0689a0c49a0dc0
duandingyang <1621424380@qq.com>
2019-06-13 17:53:47 +0800

    update

--- src/main/java/cn/ac/iie/authorization/annotation/AuthToken.java
+++ src/main/java/cn/ac/iie/authorization/annotation/AuthToken.java
@@ -1,4 +1,4 @@
-package cn.ac.iie.service;
+package cn.ac.iie.authorization.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- src/main/java/cn/ac/iie/controller/TokenController.java
+++ src/main/java/cn/ac/iie/controller/TokenController.java
@@ -1,18 +1,26 @@
 package cn.ac.iie.controller;
 
+import cn.ac.iie.service.UserService;
 import cn.ac.iie.vo.ResponseResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LoginController {
-    Logger logger = LoggerFactory.getLogger(LoginController.class);
+@RequestMapping("/tokens")
+public class TokenController {
+    Logger logger = LoggerFactory.getLogger(TokenController.class);
+
+    @Autowired
+    UserService userService;
 
     @PostMapping
     public ResponseResult login(@RequestParam String username, @RequestParam String password) {
+        userService.getByUserName(username);
         return null;
     }
 
